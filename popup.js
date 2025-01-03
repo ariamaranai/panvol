b391bd0434225a267448ea584c7d8b73708e15d7
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-01-03 22:15:18 +0900

    Add files via upload

--- popup.js
+++ popup.js
@@ -1,24 +1,17 @@
 (async chrome => {
+  let vol = document.body.firstChild.lastChild;
+  let pan = document.body.lastChild.lastElementChild;
   let tabId = (await chrome.tabs.query({ active: !0, currentWindow: !0 }))[0].id;
-  let streamId = ((await chrome.tabCapture.getCapturedTabs()).find(v => v.tabId == tabId)) ||
-    await chrome.tabCapture.getMediaStreamId();
+  let streamId = ((await chrome.tabCapture.getCapturedTabs()).find(v => v.tabId == tabId)) || await chrome.tabCapture.getMediaStreamId();
+  
   (await chrome.runtime.getContexts({})).length < 2
-    ? (
-      await chrome.offscreen.createDocument({
+    ? await chrome.offscreen.createDocument({
         justification: "",
         reasons: ["USER_MEDIA"],
         url: "offscreen.htm"
       })
-    )
-    : (
-      chrome.runtime.onMessage.addListener(e => (
-        vol.value = e[0],
-        pan.value = e[1]
-      )),
-      chrome.runtime.sendMessage([tabId, streamId])
-    );
-  let vol = document.body.firstChild.lastChild;
-  let pan = document.body.lastChild.lastElementChild;
+    : (chrome.runtime.onMessage.addListener(e => (vol.value = e[0], pan.value = e[1])),
+       chrome.runtime.sendMessage([tabId, streamId]));
   vol.oninput = e => chrome.runtime.sendMessage([tabId, streamId, +e.target.value]);
   pan.oninput = e => chrome.runtime.sendMessage([tabId, streamId, e.target.value]);
 })(chrome);
