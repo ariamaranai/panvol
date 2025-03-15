7283fa9d3928edc070bb9a274573ebc73341e5f7
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-03-15 15:34:06 +0900

    Add files via upload

--- offscreen.js
+++ offscreen.js
@@ -1,20 +1,16 @@
 {
   let audioNodes = {};
-  chrome.runtime.onMessage.addListener(e => {
-    let tabId = e[0];
-    let streamId = e[1];
-    let audioNode = audioNodes[tabId];
+  chrome.runtime.onMessage.addListener((e, _, r) => {
+    let audioNode = audioNodes[e[0]];
     audioNode
       ? e.length > 2
-        ? typeof e[2] == "number"
-          ? audioNode[0].value = e[2]
-          : audioNode[1].value = +e[2]
-        : chrome.runtime.sendMessage([audioNode[0].value, audioNode[1].value])
+        ? audioNode[e[3]].value = e[2]
+        : r([audioNode[0].value, audioNode[1].value])
       : navigator.mediaDevices.getUserMedia({
           audio: {
             mandatory: {
               chromeMediaSource: "tab",
-              chromeMediaSourceId: streamId
+              chromeMediaSourceId: e[1]
             }
           }
         }).then(media => {
@@ -24,12 +20,11 @@
           context.createMediaStreamSource(media).connect(gainNode);
           gainNode.connect(panNode);
           panNode.connect(context.destination);
-          audioNode = audioNodes[tabId] = [gainNode.gain, panNode.pan];
-          e.length > 2 && (
-            typeof e[2] == "number"
-              ? audioNode[0].value = e[2]
-              : audioNode[1].value = +e[2]
-          );
-        });
+          audioNode = audioNodes[e[0]] = [gainNode.gain, panNode.pan];
+          e.length > 2
+            ? audioNode[e[3]].value = e[2]
+            : r(0)
+        }).catch(() => 0);
+      return !0
   });
 }
--- popup.js
+++ popup.js
@@ -1,23 +1,20 @@
-chrome.runtime.getContexts({}, async contexts => {
-  let body = document.body;
-  let vol = body.firstChild.lastChild;
-  let pan = body.lastChild.lastElementChild;
-  let tabId = (await chrome.tabs.query({ active: !0, currentWindow: !0 }))[0].id;
-  let streamId = ((await chrome.tabCapture.getCapturedTabs()).find(v => v.tabId == tabId)) ||
-    await chrome.tabCapture.getMediaStreamId();
-  contexts.length < 2
-    ? chrome.offscreen.createDocument({
+chrome.runtime.getContexts({}, contexts =>
+  chrome.tabs.query({ active: !0, currentWindow: !0 }, async tabs => {
+    let tabId = tabs[0].id;
+    let streamId = (await chrome.tabCapture.getCapturedTabs()).find(v => v.tabId == tabId) || await chrome.tabCapture.getMediaStreamId();
+    let body = document.body;
+    let vol = body.firstChild.lastChild;
+    let pan = body.lastChild.lastElementChild;
+    contexts.length < 2
+      ? chrome.offscreen.createDocument({
         justification: "",
         reasons: ["USER_MEDIA"],
         url: "offscreen.htm"
       })
-    : (
-      chrome.runtime.onMessage.addListener(e => (
-        vol.value = e[0],
-        pan.value = e[1]
-      )),
-      chrome.runtime.sendMessage([tabId, streamId])
-    );
-  vol.oninput = e => chrome.runtime.sendMessage([tabId, streamId, +e.target.value]);
-  pan.oninput = e => chrome.runtime.sendMessage([tabId, streamId, e.target.value]);
-});
+      : chrome.runtime.sendMessage([tabId, streamId], m =>
+         m && (vol.value = m[0], pan.value = m[1])
+      );
+    vol.oninput = e => chrome.runtime.sendMessage([tabId, streamId, +e.target.value, 0]);
+    pan.oninput = e => chrome.runtime.sendMessage([tabId, streamId, +e.target.value, 1]);
+  })
+);
