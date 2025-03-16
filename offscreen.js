d55e82058e40dbc7cdd5174c5e44fc0d057e57b0
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-03-16 09:50:15 +0900

    Add files via upload

--- offscreen.js
+++ offscreen.js
@@ -1,16 +1,16 @@
 {
   let audioNodes = {};
-  chrome.runtime.onMessage.addListener((e, _, r) => {
-    let audioNode = audioNodes[e[0]];
+  chrome.runtime.onMessage.addListener((m, _, r) => {
+    let audioNode = audioNodes[m[0]];
     audioNode
-      ? e.length > 2
-        ? audioNode[e[3]].value = e[2]
+      ? m.length > 2
+        ? audioNode[m[3]].value = m[2]
         : r([audioNode[0].value, audioNode[1].value])
       : navigator.mediaDevices.getUserMedia({
           audio: {
             mandatory: {
               chromeMediaSource: "tab",
-              chromeMediaSourceId: e[1]
+              chromeMediaSourceId: m[1]
             }
           }
         }).then(media => {
@@ -20,9 +20,9 @@
           context.createMediaStreamSource(media).connect(gainNode);
           gainNode.connect(panNode);
           panNode.connect(context.destination);
-          audioNode = audioNodes[e[0]] = [gainNode.gain, panNode.pan];
-          e.length > 2
-            ? audioNode[e[3]].value = e[2]
+          audioNode = audioNodes[m[0]] = [gainNode.gain, panNode.pan];
+          m.length > 2
+            ? audioNode[m[3]].value = m[2]
             : r(0);
         }).catch(() => 0);
       return !0
